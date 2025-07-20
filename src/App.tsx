5162920fa8c00230cb381cb5d37a8da3e393abf6
gaurav pandey <128570258+Gauravpandey001@users.noreply.github.com>
2025-07-20 21:20:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 // src/App.tsx
 import { useState } from 'react';
-import LandingPage from './pages/LandingPage';
-import NegotiationPage from './pages/NegotiationPage';
 
 function App() {
-  const [session, setSession] = useState(null);
-
-  if (!session) {
-    return <LandingPage onRoomJoin={setSession} />;
-  }
-
-  return <NegotiationPage session={session} />;
 }
 
 export default App;
