73870975e82a73af0fbd5b5eb0cd9493987117ae
gaurav pandey <128570258+Gauravpandey001@users.noreply.github.com>
2025-07-21 01:36:37 +0530

    Update connect.js

--- backend/database/connect.js
+++ backend/database/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URL;
+    const mongoURI = process.env.MONGO_URI;
     
     if (!mongoURI) {
       throw new Error("❌ MONGO_URL is not defined in environment variables");
